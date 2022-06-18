a0604cce681e6dbfb5298b7cc0dfde4e26c93847
auoia <ptaototo123@gmail.com>
2022-06-18 01:58:40 -0700

    handle error or result from createUser

--- routes/initial.js
+++ routes/initial.js
@@ -1,17 +1,34 @@
+const Users = require("../models/users");
 const express = require("express");
 
-const app = express();
+const router = express.Router();
+// Users(saltRounds, minUsernameLength, minPasswordLength)
+const User = new Users(12, 4, 6);
 
-app.get("/signup", (req, res) => {
+router.get("/signup", (req, res) => {
 	res.render("signup");
 });
 
-app.post("/signup", (req,res, next) => {
-	
-
+router.post("/signup", (req, res, next) => {
 	const username = req.body.username;
+	const email = req.body.email;
 	const password = req.body.password;
 
+	User.createUser(username, email, password, (err, result) => {
+		if (err) {
+			console.log(err.message)
+			return;
+		}
+
+		console.log("result: " + result);
+	});
+	// Promise.resolve(resultCreateUser).then(console.log(resultCreateUser));
+	// 	console.log(val);
+	// }, function(val) {
+	// 	console.log(val);
+	// })
 
 	return res.redirect("/signup");
 });
+
+module.exports = router;
